b5948814adceb481da29f68cc83b887f05e97edf
BMriver <133412692+BMriver@users.noreply.github.com>
2023-11-26 19:24:17 -0600

    Update OpStack.java

--- src/main/java/drive/OpStack.java
+++ src/main/java/drive/OpStack.java
@@ -16,8 +16,26 @@ public class OpStack extends Stack {
         return result;
     }
 
-    @Override
+    //Returns the operator at the top of the stack without removing it
     public Node peek() {
         return this.Head;
+    }
+    // Checks if a character is an operator
+    public boolean isOperator(char c) {
+        return Ops.indexOf(c) != -1;
+    }
+
+    // Returns the precedence of an operator
+    public int precedence(String op) {
+        switch (op) {
+            case "+":
+            case "-":
+                return 1;
+            case "*":
+            case "/":
+                return 2;
+            default:
+                return -1;
+        }
     }
 }
